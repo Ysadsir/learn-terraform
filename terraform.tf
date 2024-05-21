01d75a8920a90da43d9fad2495ccf50ee700b78a
Yves-Guillaume HERVE <yg.herve@gmail.com>
2024-05-21 14:33:35 +0200

    Remove cloud block

--- terraform.tf
+++ terraform.tf
@@ -2,14 +2,15 @@
 # SPDX-License-Identifier: MPL-2.0
 
 terraform {
-
+/*
   cloud {
-    organization = "organization-name"
+    organization = "La_Capsule"
 
     workspaces {
       name = "learn-terraform"
     }
   }
+*/
 
   required_providers {
     aws = {
